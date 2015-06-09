6488aeb95010a752e5059e2dca465946244c9b28
dfieldsarlington <dfieldsarlington@gmail.com>
2015-06-09 18:52:01 -0400

    updated scripts

---
+++ style.js
@@ -0,0 +1,20 @@
+ #panel, #flip {
+            padding: 2px;
+            text-align: center;
+            background-color: #f2dcdb;
+            border: solid 1px #c0504d;
+            max-width: 1000%
+            }
+            #panel {
+            padding: 2px;
+            display: none;
+            }
+            #main {
+            max-width: 100%;
+            text-align: left;
+            }
+            #map {
+            text-align: center;
+            background-color: #f2dcdb;
+            border: solid 1px #c0504d;
+            }
